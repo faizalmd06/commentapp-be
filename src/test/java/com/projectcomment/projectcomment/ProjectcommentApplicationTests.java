4cf2962f5cd94e12d5942ff5e80cdd2afa98cb85
faizalmd06 <faizalrafeeq@gmail.com>
2022-02-12 09:00:19 +0530

    first commit

---
+++ src/test/java/com/projectcomment/projectcomment/ProjectcommentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectcomment.projectcomment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectcommentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
